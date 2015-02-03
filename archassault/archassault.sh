a05ddc8d9b2812b1a3ca8dccef69220befd7abe9
Andrea Brancaleoni <miwaxe+git@gmail.com>
2015-02-03 22:15:07 +0100

    Added archlinuxfr during build time

--- archassault/archassault.sh
+++ archassault/archassault.sh
@@ -4,7 +4,9 @@
 #   base.sh
 
 cp /etc/pacman.conf /etc/pacman.conf.backup 
-printf '\n\n[archassault]\nServer = http://repo.archassault.org/archassault/$repo/os/$arch\n\n[multilib]\nInclude = /etc/pacman.d/mirrorlist' | tee --append /etc/pacman.conf     
+printf '\n\n[archassault]\nServer = http://repo.archassault.org/archassault/$repo/os/$arch\n\n[multilib]\nInclude = /etc/pacman.d/mirrorlist' | tee --append /etc/pacman.conf
+printf '\n\n[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/$arch' | tee --append /etc/pacman.conf
+
 timeout 10 dirmngr
 pacman-key -r CC1D2606 && pacman-key --lsign-key CC1D2606
 

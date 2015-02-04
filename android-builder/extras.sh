b8df1e31ba90c22c1ddee76d3451795c741b2516
Andrea Brancaleoni <miwaxe+git@gmail.com>
2015-02-04 18:59:54 +0100

    Added xauth

--- android-builder/extras.sh
+++ android-builder/extras.sh
@@ -2,6 +2,6 @@
 export DEBIAN_FRONTEND=noninteractive
 # Prepare Extras
 ## Installs Utilities
-apt-get install -q -y htop dtach tig libxi6 libswt-gtk-3-java device-tree-compiler yasm gettext
+apt-get install -q -y htop dtach tig libxi6 libswt-gtk-3-java device-tree-compiler yasm gettext xauth
 mkdir -p /home/vagrant/.swt/lib/linux/x86_64/
 ln -s /usr/lib/jni/libswt-* /home/vagrant/.swt/lib/linux/x86_64/

1449acc47ee284de5f88451e31c08a452686bd15
Andrea Brancaleoni <miwaxe+git@gmail.com>
2015-02-08 01:39:38 +0100

    SSH Fix

--- archbsd/base.sh
+++ archbsd/base.sh
@@ -54,5 +54,5 @@ root ALL=(ALL) ALL
 %wheel ALL=(ALL) NOPASSWD: ALL
 EOF
 
-echo vagrant | pw user add -n vagrant -G wheel -c "Vagrant User" -m -s /usr/bin/bash
+echo vagrant | pw user add -n vagrant -G wheel -c "Vagrant User" -m -s /usr/bin/bash -h 0
 ENDCHROOT
